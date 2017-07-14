26ab85f2a2e67f85352445b2fe6820a2d9dd44e4
Derek Webb <derek.webb@720strategies.com>
2017-07-14 16:21:35 -0400

    Added heros list.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,27 +6,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
-        this.title = 'Tour of Heros';
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
-    }
-    return AppComponent;
-}());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        template: "\n        <h1>{{title}}</h1>\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id: </label>{{hero.id}}</div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n        </div>\n    "
-    })
-], AppComponent);
-exports.AppComponent = AppComponent;
 var Hero = (function () {
     function Hero() {
     }
     return Hero;
 }());
 exports.Hero = Hero;
+var HEROES = [
+    { id: 1, name: 'Mr. Nice' },
+    { id: 2, name: 'Narco' },
+    { id: 3, name: 'Bombasto' },
+    { id: 4, name: 'Celeritas' },
+    { id: 5, name: 'Magneta' },
+    { id: 6, name: 'RubberMan' },
+    { id: 7, name: 'Dynama' },
+    { id: 8, name: 'Dr IQ' },
+    { id: 9, name: 'Magma' },
+    { id: 10, name: 'Tornado' }
+];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Tour of Heros';
+        this.heroes = HEROES;
+        this.selectedHero = false;
+    }
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n        <h1>{{title}}</h1>\n        <h2>My Heroes</h2>\n        <ul class=\"heroes\">\n            <li *ngFor=\"let hero of heroes\" (click)=\"onSelect(hero)\">\n                <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n            </li>\n        </ul>\n        <div *ngIf=\"selectedHero\">\n            <h2>{{selectedHero.name}} details!</h2>\n            <div><label>id: </label>{{selectedHero.id}}</div>\n            <div>\n                <label>name: </label>\n                <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n            </div>\n        </div>\n    ",
+        styles: ["\n        .selected {\n            background-color: #CFD8DC !important;\n            color: white;\n        }\n        .heroes {\n            margin: 0 0 2em 0;\n            list-style-type: none;\n            padding: 0;\n            width: 15em;\n        }\n        .heroes li {\n            cursor: pointer;\n            position: relative;\n            left: 0;\n            background-color: #EEE;\n            margin: .5em;\n            padding: .3em 0;\n            height: 1.6em;\n            border-radius: 4px;\n        }\n        .heroes li.selected:hover {\n            background-color: #BBD8DC !important;\n            color: white;\n        }\n        .heroes li:hover {\n            color: #607D8B;\n            background-color: #DDD;\n            left: .1em;\n        }\n        .heroes .text {\n            position: relative;\n            top: -3px;\n        }\n        .heroes .badge {\n            display: inline-block;\n            font-size: small;\n            color: white;\n            padding: 0.8em 0.7em 0 0.7em;\n            background-color: #607D8B;\n            line-height: 1em;\n            position: relative;\n            left: -1px;\n            top: -4px;\n            height: 1.8em;\n            margin-right: .8em;\n            border-radius: 4px 0 0 4px;\n        }\n    "]
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
