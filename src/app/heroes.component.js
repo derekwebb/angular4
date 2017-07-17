520d6dae250d88f22db7ae1c307e18a684d1b4b0
Derek Webb <derek.webb@720strategies.com>
2017-07-17 17:34:50 -0400

    More reworking of the heroes test app.

--- src/app/heroes.component.js
+++ src/app/heroes.component.js
@@ -17,7 +17,7 @@ var HeroesComponent = (function () {
     HeroesComponent.prototype.getHeroes = function () {
         var _this = this;
         this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
-        //this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+        // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
     };
     HeroesComponent.prototype.ngOnInit = function () {
         this.getHeroes();
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -74,7 +74,7 @@ export class HeroesComponent implements OnInit {
 
     getHeroes(): void {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-        //this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+        // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
     }
 
     ngOnInit(): void {
